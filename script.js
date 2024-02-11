5c1001e01bb6c07f927238b946c3845812c17871
KatrinaZuus <katrinazuus@gmail.com>
2024-02-11 22:01:43 +0400

    script-is cvlilebebi

--- script.js
+++ script.js
@@ -2,13 +2,49 @@
 
 const btn = Array.from(document.getElementsByClassName("question"));
 const answr = Array.from(document.getElementsByClassName("answer"));
-for (i=0; i<answr.length; i++) {
-    console.log(answr[i])
-    // console.log(answr[1])
-    btn.addEventListener("click", ()=>{
-        btn[0].style.color = "green"
-        console.log(btn[0])
-    })
-}
+const divQuestionAnsver = Array.from(document.getElementsByClassName("q-a"))
+
+let questionAnsver = "none"
+
+// for (i=0; i<=answr.length; i++) {
+
+//   btnClick=btn[i]
+//   questionAnsver=divQuestionAnsver[i].nextElementSibling
+//   console.log(btnClick)
+//   console.log(questionAnsver)
+   
+//   btnClick.addEventListener("click", ()=>{
+//     questionAnsver.style.display = "block"
+//   })
+
+//   btn[i].addEventListener("click", ()=>{
+//     answr[0].style.display="block"
+//    })
 
 
+// }
+
+answerItem = "none"
+
+btn.forEach ((button)=>{
+  button.addEventListener("click", ()=>{
+    answerItem = button.parentElement.nextSibling
+    console.log(button.parentElement)
+    console.log(answerItem
+      )
+    button.parentElement.nextSibling.style.display = "block"
+  })
+})
+
+
+
+
+ 
+
+
+   
+
+  
+
+// TypeError: Cannot read properties of undefined (reading 'nextElementSibling')
+// TypeError: Cannot read properties of undefined (reading 'nextElementSibling')
